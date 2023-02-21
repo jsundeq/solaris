074d4dc7ec3a065884f82c8f0bed3a8ff9f7daa9
jsundeq <116060345+jsundeq@users.noreply.github.com>
2023-02-21 16:54:29 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ async function getSolarSystem() {
 
 getSolarSystem();
 
+// const searchBox = document.getElementById("planet")
+//     console.log(searchBox);
+
 function renderPlanet(id) {
     const planet = planets.find(planet => planet.id===id);
     const lightBox = document.getElementById("light-box");
@@ -23,50 +26,41 @@ function renderPlanet(id) {
 
     lightBox.innerHTML += `
     <header class='planet-title'>
-    <h1>${planet.name}</h1>
-    <h2>${planet.latinName}</h2>
-      </header>
-      <div class="planet-info-wrapper">
-      <section class="planet-info">
-          <p>${planet.desc}</p>
-      </section>
-      
-  <hr>
-  
-        <section class="planet-info-grid">
-            <article>
-                <h4>OMKRETS</h4>
-                <p class="grid-info">${planet.circumference} km</p>
-            </article>
-
-            <article>
-                <h4>KM FRÅN SOLEN</h4>
-                <p class="grid-info">${planet.distance} km</p>
-            </article>
-
-            <article>
-                <h4>MAX TEMPERATUR</h4>
-                <p class="grid-info">${planet.temp.day}C</p>
-            </article>
-
-            <article>
-                <h4>MIN TEMPERATUR</h4>
-                <p class="grid-info">${planet.temp.night}C</p>
-            </article>
+        <h1>${planet.name}</h1>
+        <h2>${planet.latinName}</h2>
+    </header>
+    
+    <div class="planet-info-wrapper">
+        <section class="planet-info">
+            <p>${planet.desc}</p>
         </section>
-    
+        <section class="planet-info-grid">
+            <article class="grid-info1">
+                <h4>OMKRETS</h4>
+                <p>${planet.circumference} km</p>
+            </article>
 
-    <hr>
+            <article class="grid-info2">
+                <h4>KM FRÅN SOLEN</h4>
+                <p>${planet.distance} km</p>
+            </article>
+
+            <article class="grid-info3">
+                <h4>MAX TEMPERATUR</h4>
+                <p>${planet.temp.day}C</p>
+            </article>
+
+            <article class="grid-info4">
+                <h4>MIN TEMPERATUR</h4>
+                <p>${planet.temp.night}C</p>
+            </article>
     
-        <section class="planet-moons">
-            <article>
-            <h4>MÅNAR</h4>
-            <p class="grid-info">${moonString}</p>
-        </article>
+            <article class="grid-info5">
+                <h4>MÅNAR</h4>
+                <p">${moonString}</p>
+            </article>
         </section>
     </div>
-
-</main>
       `;
       lightBox.appendChild(text);
     
